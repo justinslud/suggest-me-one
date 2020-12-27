8fa8df5aa472d2bd2b67bc35fa3988ce40010cf0
Justin Slud <justinslud@yahoo.com>
2020-12-27 08:19:02 -0500

    top 100 genres and subjects

---
+++ data.py
@@ -0,0 +1,204 @@
+GENRES = """Astronomy
+Ballet
+Business
+Children's literature
+Constrained writing
+Cultural Revolution
+Cultural relativism
+Economic history
+English Renaissance theatre
+Environmental economics
+Fact
+Fairy tale
+Future history
+Gender studies
+Globalization
+Historical fiction
+History of the Caribbean
+Hoax
+Humour
+Indigenous peoples
+Journal
+Libretto
+Literary criticism
+Morality
+Music history
+Natural science
+Nuclear warfare
+Ornithology
+Parenting
+Philosophy of science
+Political economy
+Politics of Turkey
+Politics of the United States
+Postcyberpunk
+Prose
+Quran
+Robbery
+Rococo
+Science fiction fandom
+Security
+Semantics
+Society
+Steganography
+Strategy
+Supernatural
+Surrealism
+Sword and sorcery
+Taoism
+Technology
+Terrorism
+Thelema
+Theodicy
+Theology
+Thesaurus
+Time travel
+Tourism
+Tragedy
+Transhumanism
+Unemployment
+Unification Church
+Utopia
+Utopian and dystopian fiction
+Vampire
+Vegetarian cuisine
+Vietnam War
+War
+Wargaming
+Wicca
+William Shakespeare
+Wine
+Wisdom
+Witchcraft
+World War II
+Wuxia
+Yoga
+Zoology
+Cryptography
+Bizarro
+Bizarro fiction
+Christian devotional literature
+Comedy-drama
+Consultant
+Diary
+Drama
+Drama (genre)
+Fantastic
+Fiction
+Field guide
+Ghost story
+Guide book
+Melodrama
+Memento mori
+Mythopoeia
+Communism
+Psychedelic literature
+Realism (arts)
+Time travel in fiction
+Christian mysticism
+Comic novel
+Fantastique
+""".split('\n')
+
+
+SUBJECTS = """American science fiction novels
+Debut novels
+American fantasy novels
+American young adult novels
+HarperCollins books
+Greek New Testament minuscules
+American novels adapted into films
+2000s science fiction novels
+Novels first published in serial form
+American children's novels
+English-language books
+Doubleday (publisher) books
+American non-fiction books
+Historical novels
+1990s science fiction novels
+Fantasy novels
+British novels adapted into films
+2007 books
+Children's picture books
+2006 books
+Novels set in New York City
+Novels set in London
+Tor Books books
+2008 books
+Alfred A. Knopf books
+English novels
+2004 books
+Works published under a pseudonym
+2005 books
+British science fiction novels
+British children's novels
+2009 books
+Del Rey books
+2010 books
+Children's fantasy novels
+2003 books
+Viking Press books
+Sequel novels
+World War II novels
+Fantasy short story collections
+American historical novels
+2011 books
+2007 American novels
+Random House books
+Dystopian novels
+Jonathan Cape books
+American thriller novels
+2006 American novels
+Ace Books books
+Greek New Testament lectionaries
+2008 American novels
+12th-century biblical manuscripts
+Philosophy books
+2001 books
+2004 American novels
+1980s science fiction novels
+2005 American novels
+19th-century British novels
+Single-writer short story collections
+2009 American novels
+Essay collections
+Hodder & Stoughton books
+2002 books
+2003 American novels
+Faber and Faber books
+20th-century Australian novels
+Children's historical novels
+2012 non-fiction books
+1998 books
+1999 books
+11th-century biblical manuscripts
+Heinemann (publisher) books
+American biographies
+Greek New Testament uncials
+13th-century biblical manuscripts
+Children's science fiction novels
+British fantasy novels
+2000 books
+Thriller novels
+Economics books
+American children's books
+1970s science fiction novels
+Political books
+2000s fantasy novels
+2006 novels
+2010 American novels
+Ballantine Books books
+Little, Brown and Company books
+2001 American novels
+English-language novels
+Crime novels
+Houghton Mifflin books
+Time travel novels
+2008 novels
+American horror novels
+DAW Books books
+2004 novels
+2002 American novels
+1997 books
+Science fiction anthologies
+""".split('\n')
